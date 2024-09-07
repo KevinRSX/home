9d8e0834f7f29a5b33476f89d3a080ac4a91884b
kevinxue <kevinxue@node0.kevinxue-216210.gaia-pg0.utah.cloudlab.us>
2024-09-06 22:55:16 -0600

    Add tmux

--- install.sh
+++ install.sh
@@ -9,6 +9,12 @@ install_omz() {
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 }
 
+install_omz_plugin() {
+  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+  git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install
+}
+
 install_tmux() {
   rm -rf /tmp/tmux
   git clone https://github.com/tmux/tmux /tmp/tmux
@@ -17,6 +23,10 @@ install_tmux() {
   ./configure && make
   cp /tmp/tmux/tmux $HOME/bin/
   popd
+}
+
+install_tpm() {
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 }
 
 if test -f "~/.vimrc"; then
@@ -30,7 +40,15 @@ fi
 
 echo ">>>>>>>>Installing Oh My Zsh<<<<<<<<"
 install_omz
+cp .zshrc $HOME/.zshrc
+cp .oh-my-zsh/kevin.zsh-theme $HOME/.oh-my-zsh/themes/
+# install_omz_plugin
 
 echo ">>>>>>>>Installing TMUX<<<<<<<<"
-install_tmux
+# install_tmux
+# install_tpm
+cp .tmux.conf $HOME/.tmux.conf
 
+
+echo ">>>>>>>>Installing vim<<<<<<<<"
+cp .vimrc $HOME/.vimrc
